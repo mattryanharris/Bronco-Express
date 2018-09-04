69132861cb445c92aa2607d4df81883be96c7c86
mattryanharris <matthew@biofare.org>
2018-09-03 21:52:25 -0700

    Imported click, start of the actual project

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def main():
-	print("Test")
+import click
 
-main()
+@click.command()
+@click.option('--route', prompt="What route",help="Defines which bus route student is taking.")
+
+
+def test():
+    """Bronco Express is a CLI application for CS3800.  This program helps student find the arrival times for the Bronco Express shuttle.  Users will apply two flags to info the program which route and stop are at."""
+
+if __name__ == '__main__':
+    test()
