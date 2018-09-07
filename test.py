129f8f737c47546575e6ea013b4c60c5663a61ca
mattryanharris <matthew@biofare.org>
2018-09-06 19:44:16 -0700

    Addition of route A with an overflow parking lot stop for testing purposes

--- test.py
+++ test.py
@@ -13,6 +13,12 @@ if stop == "RESIDENTS HALL":
 if stop == "BUILDING 94":
 	stop = 33803
 
+if route == 'A':
+	route = 3164
+
+if stop == "OVERFLOW PARKING LOT":
+	stop = 1592066
+
 url = "https://www.broncoshuttle.com/Route/%s/Stop/%s/Arrivals?customerID=21" % (route, stop)
 
 response = req.urlopen(url)
